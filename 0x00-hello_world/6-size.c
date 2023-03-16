4661e45ad5c4a4448843df5f22a03c87dffd00f2
Emilymasisa <nyamasyomasisa@gmail.com>
2023-03-16 05:13:18 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)"\n", sizeof(char));
+	printf("Size of int: %lu byte(s)"\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)"\n", sizeof(long int));
+	printf("size of a long long: %lu byte(s)"\n", sizeof(long long int));
+	printf("size of fruit: %lu byte(s)"\n", sizeof(fruit));
+	Return (0);
+			
+}	
