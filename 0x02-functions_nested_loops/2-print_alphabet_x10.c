4d4d8789be3976796c7829428caf4070ca89fd91
Emilymasisa <nyamasyomasisa@gmail.com>
2023-03-28 04:40:55 -0700

    alphabet2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Nothing on success.
+ */
+void print_alphabet(void)
+{
+	int start = 97, end = 122;
+
+	while (start <= end)
+	(
+	 _putchar(start);
+	 start**
+	)
+	_putchar('\n');
+}
+        		
