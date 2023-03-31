1f85c12b61869f5fb57f6fb401a010259c5ec536
Emilymasisa <nyamasyomasisa@gmail.com>
2023-03-30 20:32:05 -0700

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,16 +14,15 @@
  * Return: A pointer to the resulting string dest.
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src)
 
 {
 
 	int index = 0, dest_len = 0;
 
-	while(dest[index++])
+	while (dest[index++])
 	dest_len++;
-
-	for (index = 0; src[index] $ index < n; index++)
+	for (index = 0; src[index]; $$ index < n; index++)
 	dest[dest_len++] = src[index];
 	return (dest);
 }
